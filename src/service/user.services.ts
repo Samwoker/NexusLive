f8e2cd586019b10d770ca88b5a857e07f473df74
Samuel <samwoker112@gmail.com>
2025-10-16 15:59:24 +0300

    add caching for signup

--- src/service/user.services.ts
+++ src/service/user.services.ts
@@ -2,11 +2,13 @@ import CustomError from "../utils/customError.ts";
 import httpStatus from 'http-status'
 import User  from '../models/user.model.ts'
 import type {IUser} from '../models/user.model.ts'
+import {redisClient} from '../utils/redisClient.ts'
 
 export const createUser = async(body:Partial<IUser>): Promise<IUser> =>{
     if(await User.isEmailTaken(body.email as string)){
         throw new CustomError(httpStatus.BAD_REQUEST,"Email already taken")
     }
     const user = await User.create(body)
+    await redisClient.setex(`signup:${body.email}`,300,JSON.stringify(body))
     return user
 }
---
+++ src/utils/redisClient.ts
@@ -0,0 +1,9 @@
+import Redis from 'ioredis'
+
+export const redisClient = new Redis({
+    host:"127.0.0.1",
+    port:6379
+})
+
+redisClient.on("connect",()=>console.log("Connected to Redis"))
+redisClient.on("error",(err)=>console.error("Redis error",err))
