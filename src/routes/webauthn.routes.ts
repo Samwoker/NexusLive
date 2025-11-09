3c6952c47f5e50e6b771fd92204001565e9114f2
Samuel <samwoker112@gmail.com>
2025-11-09 12:46:36 +0300

    Implement webauthn register endpoint

--- src/routes/webauthn.routes.ts
+++ src/routes/webauthn.routes.ts
@@ -5,6 +5,7 @@ import * as webauthnController from "../controllers/webauthn.controllers.ts"
 const router = express.Router()
 
 router.post("/register-options",webauthnController.registerOptions)
+router.post("/register",authLimiter,webauthnController.register)
 
 
 export default router;
