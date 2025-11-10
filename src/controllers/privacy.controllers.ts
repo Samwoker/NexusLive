efee943533772d5be4c6e456f524de087ee66ce7
Samuel <samwoker112@gmail.com>
2025-11-10 22:17:34 +0300

    Implementing getUserPrivacyController

---
+++ src/controllers/privacy.controllers.ts
@@ -0,0 +1,10 @@
+import catchAsync from "../utils/catchAsync.ts";
+import httpStatus from "http-status";
+import type { Request, Response } from "express";
+import type { IUser } from "../models/user.model.ts";
+
+export const getUserPrivacy = catchAsync(async(req:Request,res:Response)=>{
+    const user:IUser = (req as any).user;
+    res.status(httpStatus.OK).json({message:"User privacy info retrieved successfully",data:{user}})
+})
+
