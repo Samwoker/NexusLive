7529e8a8a914fbec20ea8b0dd8dce0bac24761c5
Samuel <samwoker112@gmail.com>
2025-11-10 19:30:16 +0300

    Implement verifyAuthentication controller

--- src/controllers/webauthn.controllers.ts
+++ src/controllers/webauthn.controllers.ts
@@ -3,6 +3,7 @@ import httpStatus from "http-status";
 import type { Request,Response } from "express";
 import * as userService from "../service/user.services.ts"
 import * as webauthn from "../utils/webauthn.ts"
+import * as tokenService from "../service/token.services.ts"
 
 export const registerOptions = catchAsync(async(req:Request,res:Response)=>{
     const {username} = req.body;
@@ -33,5 +34,13 @@ export const authenticate = catchAsync(async(req:Request,res:Response)=>{
     const {username} = req.body;
     const user = await userService.findUserByUsername(username);
     const credential = user.credentials.find(c=>c.credentialId === req.body.credential.id)
-    const result = await webauthn.verifyAuthenticationResponses()
+    const result = await webauthn.verifyAuthenticationResponses(user,credential)
+    if(result.verified){
+        (credential as any).counter = result.authenticationInfo.newCounter;
+        await user.save()
+        const token = await tokenService.generateAuthTokens(user._id)
+        res.status(httpStatus.OK).json({token})
+    }else{
+        res.status(httpStatus.UNAUTHORIZED).json({error:"Authentication failed"})
+    }
 })
