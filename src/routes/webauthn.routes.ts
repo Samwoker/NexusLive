9b45980280ae9da65c230c63065a8be37e537079
Samuel <samwoker112@gmail.com>
2025-11-10 19:33:18 +0300

    Implement webauthn authenticate-options endpoint

--- src/routes/webauthn.routes.ts
+++ src/routes/webauthn.routes.ts
@@ -6,6 +6,7 @@ const router = express.Router()
 
 router.post("/register-options",webauthnController.registerOptions)
 router.post("/register",authLimiter,webauthnController.register)
+router.post("/authenticate-options",webauthnController.authenticateOptions);
 router.post("/authenticate",webauthnController.authenticate)
 
 
