7612b03d54261d450a4548942584fcedea12919c
Samuel <samwoker112@gmail.com>
2025-11-13 15:04:33 +0300

    Implement upload avatar route

--- src/routes/profile.routes.ts
+++ src/routes/profile.routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import * as profileController from "../controllers/profile.controllers.ts"
 import authenticate from "../middlewares/auth.ts";
+import {upload} from "../config/multer.ts"
 
 const router = express.Router();
 
@@ -9,6 +10,6 @@ router.patch("/update-profile",authenticate,profileController.updateProfile)
 router.patch("/change-password",authenticate,profileController.changePassword)
 router.delete("/delete-account",authenticate,profileController.deleteAccount)
 router.post("/restore-account",authenticate,profileController.recoverAccount)
-
+router.post("upload-avatar",authenticate,upload.single("image"),profileController.uploadAvatar)
 
 export default router;
