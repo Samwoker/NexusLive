0565e3cb5632c05a31355d7576dc6b203fcb1248
Samuel <samwoker112@gmail.com>
2025-11-13 14:31:44 +0300

    implemented multer config file

---
+++ src/config/multer.ts
@@ -0,0 +1,16 @@
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "./cloudinary";
+
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params:async(req,file)=>{
+        return {
+            folder:"uploads",
+            allowed_formats:["jpg","png","jpeg","webp"],
+            public_id:file.originalname.split(".")[0]
+        }
+    }
+})
+
+export const upload = multer({storage});
